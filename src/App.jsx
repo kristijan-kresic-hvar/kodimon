7dae318c36bf36af59d0d0fb8e86b56df8951f0c
kristijan-kresic-hvar <kristijan.kresic.dev@gmail.com>
2022-11-25 21:37:39 +0100

    Initial arena setup

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,13 @@ import React from 'react'
 import styles from './App.module.css'
 
 import Home from './components/Home/Home'
+import Arena from './components/Arena/Arena'
 
 const App = () => {
   return (
     <div>
-      <Home />
+      {/* <Home /> */}
+      <Arena />
     </div>
   )
 }
---
+++ src/components/Actions/Actions.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import styles from './Actions.module.css'
+
+const Actions = () => {
+    return (
+        <div>Actions</div>
+    )
+}
+
+export default Actions
